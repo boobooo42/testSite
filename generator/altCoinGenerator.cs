8dbfa24f3a1d0a5a70e8fc4e4bda2ea80a3b58dd
David Sandborgh <dsandborgh@idfbins.com>
2018-01-26 08:00:21 -0700

    hi

---
+++ generator/altCoinGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testSite.Generator 
+{
+    public class altCoinGenerator
+    {
+        public void generateAltCoinTitle()
+        {
+            
+        }
+
+
+    }
+}
